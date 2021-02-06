369c31fe28b278f08eecc7bdaad547e8cf90fc73
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2021-02-06 18:28:50 +0200

    added Reports.Crypto.WebService solution with default projects

---
+++ WebService/Reports.Crypto.WebService.API/Controllers/CryptocurrenciesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Reports.Crypto.WebService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptocurrenciesController : ControllerBase
+    {
+        private readonly ILogger<CryptocurrenciesController> _logger;
+
+        public CryptocurrenciesController(ILogger<CryptocurrenciesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+    }
+}
